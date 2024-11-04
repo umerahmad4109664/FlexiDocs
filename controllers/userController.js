714e8c0e618bf6995ba4de724bacf3b582aaa79e
umerahmad4109664 <134127374+umerahmad4109664@users.noreply.github.com>
2024-11-04 21:53:12 +0500

    login api created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,51 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
 
 exports.createAccount = async (req, res) => {
-    const {name,email,password} = req.body
-    try {
-      const existUser = await User.findOne({ email });
+  const { name, email, password } = req.body
+  try {
+    const existUser = await User.findOne({ email });
 
-      if(existUser){
-        return res.status(409).json({ message: "User already exists" });
-      }
-      
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-        const user = new User({
-          email,
-          name,
-          password:hashedPassword
-        });
-        const savedUser = await user.save();
-        res.status(200).send(user);
-    } catch (error) {
-      console.log(error)
-      return res.status(500).send({ message: "Failed" });
+    if (existUser) {
+      return res.status(409).json({ message: "User already exists" });
     }
-  };
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      email,
+      name,
+      password: hashedPassword
+    });
+    const savedUser = await user.save();
+    res.status(200).send(user);
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({ message: "Failed" });
+  }
+};
+
+exports.signIn = async (req, res) => {
+  const { email, password } = req.body
+  try {
+
+    const user = await User.findOne({ email })
+    if (!user) return res.status(404).send({ message: "Invalid Credentials" })
+
+    const matchedPassword = await bcrypt.compare(password, user.password)
+    if (!matchedPassword) return res.status(404).send({ message: "Invalid Credentials" })
+
+    const userData = {email:user.email,name:user.name}
+    const secretKey = process.env.SECRET_KEY
+
+      const token = jwt.sign(userData, secretKey);
+
+    return res.status(200).send(token)
+
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({ message: "Failed" });
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
-const { createAccount } = require('../controllers/userController');
+const { createAccount, signIn } = require('../controllers/userController');
 
 const userRouter = require('express').Router();
 
 
 userRouter.post("/createaccount",createAccount);
+userRouter.post("/signIn",signIn);
 
 module.exports = userRouter

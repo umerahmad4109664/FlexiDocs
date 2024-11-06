30963828dd9f6a7b81de7695308c88a8c48b8981
umerahmad4109664 <134127374+umerahmad4109664@users.noreply.github.com>
2024-11-06 21:58:41 +0500

    doc start

---
+++ app/src/pages/Document.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Document = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Document
--- app/src/routes/Routes.tsx
+++ app/src/routes/Routes.tsx
@@ -1,9 +1,11 @@
     import { RouteObject } from "react-router-dom";
 import NotFound from "../components/NotFound";
 import Login from "@/pages/Login";
+import Document from "@/pages/Document";
 
     const routes:RouteObject[] = [
         { path: '/', element:<Login/>  },
+        { path: '/document', element:<Document/>  },
         { path: '*', element:<NotFound/>  },
         
     ];

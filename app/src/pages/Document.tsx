600c6eac892d70f5d859475760863bd9695799f1
umerahmad4109664 <134127374+umerahmad4109664@users.noreply.github.com>
2024-11-06 22:42:47 +0500

    text editor created

--- app/src/pages/Document.tsx
+++ app/src/pages/Document.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Document = () => {
-  return (
-    <div>
-    </div>
-  )
+    const [editorContent, setEditorContent] = useState<string>("");
+
+    const handleChange = (value: string) => {
+      setEditorContent(value);
+    };
+
+    const modules = {
+        toolbar: [
+          [{ header: "1" }, { header: "2" }, { font: [] }],
+          [{ list: "ordered" }, { list: "bullet" }],
+          ["bold", "italic", "underline"],
+          ["link"],
+        //   [{ align: "left" }, { align: "center" }, { align: "right" }, { align: "justify" }],
+          [{ align: [] }],
+          ["image"],
+        ],
+      };
+  
+    return (
+      <div className="editor-container">
+        <ReactQuill
+          value={editorContent}
+          onChange={handleChange}
+          theme="snow"
+          modules={modules}
+
+        />
+      </div>
+    );
 }
 
 export default Document
